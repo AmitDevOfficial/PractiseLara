f45399d424f724e92d3e4696206147f2b860bf85
Amit Vishwakarma <vishamit8827@gmail.com>
2025-11-06 22:36:53 +0530

    parctise

---
+++ database/migrations/2025_11_06_165706_create_client_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('client', function (Blueprint $table) {
+            $table->id();
+            $table->string('client_id', 65);
+            $table->string('fullName', 100);
+            $table->string('email', 50)->unique();
+            $table->string('password',255);
+            $table->enum('gender',['M','F']);
+            $table->string('city');
+            $table->string('image')->nullable();
+            $table->boolean('status')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('client');
+    }
+};
